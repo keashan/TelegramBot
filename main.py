d9a5b2c8adb43e8ea89b52f95c8c6e728d033c92
Keashan Jayaweera <keashanjayaweera@gmail.com>
2022-12-15 09:14:28 +0530

    Added webhook method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
-import datetime
 import requests
+from flask import Flask, request
+
+from Responses import sample_response
 from config.get_keys import Config
 
 app = Flask(__name__)
@@ -9,16 +10,14 @@ token = Config.API_KEY
 
 
 def welcome_msg(item):
-    global token
-    if item["text"].lower() == "hi":
-        msg = 'hello'
-        chat_id = item["chat"]["id"]
-        user_id = item["from"]["id"]
-        user_name = item["from"].get("username", user_id)
-        welcome_msg = '''{}'''.format(msg)
-        to_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'.format(token, chat_id,
-                                                                                                        welcome_msg)
-        resp = requests.get(to_url)
+    msg = sample_response(item["text"])
+    chat_id = item["chat"]["id"]
+    user_id = item["from"]["id"]
+    user_name = item["from"].get("username", user_id)
+    welcome_msg = '''{}'''.format(msg)
+    to_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'.format(token, chat_id,
+                                                                                                    welcome_msg)
+    resp = requests.get(to_url)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -30,3 +29,7 @@ def home():
         return {'statusCode': 200, 'body': 'Success', 'data': data}
     else:
         return {'statusCode': 200, 'body': 'Success'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
